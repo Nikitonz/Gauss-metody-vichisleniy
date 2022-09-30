faec75c734c75ba152fdbcd4a4a1d279f189f4a7
Nikitonz <100299461+Nikitonz@users.noreply.github.com>
2022-09-30 11:31:40 +0300

    check on E_matrix

--- gauss-sxema-edin-deleniya/gauss-sxema-edin-deleniya/gauss-sxema-edin-deleniya.cpp
+++ gauss-sxema-edin-deleniya/gauss-sxema-edin-deleniya/gauss-sxema-edin-deleniya.cpp
@@ -37,61 +37,52 @@ int main()
 	for (i=0; i<3; i++)
 	 for (j=0; j<3; j++)
 		ac[i][j]=mas[i][j];
-	//transpose
-	int t;
-	for (int i = 0; i < 3; ++i)
+	
+	double invdet = 1 / determinant;
+
+	double minv[3][3]; // inverse of matrix mas
+	minv[0][0] = (mas[1][1] * mas[2][2] - mas[2][1] * mas[1][2]) * invdet;
+	minv[0][1] = (mas[0][2] * mas[2][1] - mas[0][1] * mas[2][2]) * invdet;
+	minv[0][2] = (mas[0][1] * mas[1][2] - mas[0][2] * mas[1][1]) * invdet;
+	minv[1][0] = (mas[1][2] * mas[2][0] - mas[1][0] * mas[2][2]) * invdet;
+	minv[1][1] = (mas[0][0] * mas[2][2] - mas[0][2] * mas[2][0]) * invdet;
+	minv[1][2] = (mas[1][0] * mas[0][2] - mas[0][0] * mas[1][2]) * invdet;
+	minv[2][0] = (mas[1][0] * mas[2][1] - mas[2][0] * mas[1][1]) * invdet;
+	minv[2][1] = (mas[2][0] * mas[0][1] - mas[0][0] * mas[2][1]) * invdet;
+	minv[2][2] = (mas[0][0] * mas[1][1] - mas[1][0] * mas[0][1]) * invdet;
+
+	cout << "\nobratnaya matrix\n\n";
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+
+		
+			cout << minv[i][j] << ' ';
+		}
+		cout << '\n';
+	}
+
+	double **c;
+	c = new double* [3];
+	for (int i = 0; i < 3; i++)
 	{
-		for (int j = i; j < 3; ++j)
+		c[i] = new double[3];
+		for (int j = 0; j < 3; j++)
 		{
-			t = ac[i][j];
-			ac[i][j] = ac[j][i];
-			ac[j][i] = t;
+			c[i][j] = 0;
+			for (int k = 0; k < 3; k++)
+				c[i][j] += mas[i][k] * minv[k][j];
 		}
 	}
 
-	cout << "transposed matrix\n\n";
+	cout << "\nresult of multiply matrix * rev_matrix\n\n";
 	for (i = 0; i < 3; i++) {
 		for (j = 0; j < 3; j++) {
-
-			cout << ac[i][j] << ' ';
+		  if (c[i][j]<0.000'000'1)
+				  c[i][j]=0;
+			cout << c[i][j] << ' ';
 		}
 		cout << '\n';
 	}
-	//союзная матрица
-	/*00 01 02
-	  10 11 12
-	  20 21 22
-	*/
-	float souz[3][3];
-	souz[0][0] = pow(-1, 1 + 1) * (ac[1][1] * ac[2][2] - ac[1][2] * ac[2][1]);
-	souz[0][1] = pow(-1, 1 + 2) * (ac[1][0] * ac[2][2] - ac[2][0] * ac[1][2]);
-	souz[0][2] = pow(-1, 1 + 3) * (ac[1][0] * ac[2][1] - ac[1][1] * ac[2][0]);
-	souz[1][0] = pow(-1, 2 + 1) * (ac[0][1] * ac[2][2] - ac[0][2] * ac[2][1]);
-	souz[1][1] = pow(-1, 2 + 2) * (ac[0][0] * ac[2][2] - ac[0][2] * ac[2][0]);
-	souz[1][2] = pow(-1, 2 + 3) * (ac[0][0] * ac[2][1] - ac[0][1] * ac[2][0]);
-	souz[2][0] = pow(-1, 3 + 1) * (ac[0][1] * ac[1][2] - ac[1][1] * ac[0][2]);
-	souz[2][1] = pow(-1, 3 + 2) * (ac[0][0] * ac[1][2] - ac[0][2] * ac[1][0]);
-	souz[2][2] = pow(-1, 3 + 3) * (ac[1][1] * ac[0][0] - ac[1][0] * ac[0][1]);
-	cout << "\nsouz matrix\n\n";
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 3; j++) {
-
-			cout << souz[i][j] << ' ';
-		}
-		cout << '\n';
-	}
-	//-------------------------------------------------------------
-	// обратная матрица
-	cout<<"\nobratnaya matrix\n\n";
- for(i=0; i<3; i++){
-	  for (j=0;j<3;j++){
-					
-			  souz[i][j]/=determinant;
-					cout<<souz[i][j]<<' ';
-			}
-			cout<<'\n';
-	}
-
 	//=============================================================
 			cout<<"\n\ndata confirmation: \n\n";
 			cout<<" / ";
@@ -131,7 +122,7 @@ int main()
 			}
 			//перекомпановка всё
 			//вычисления
-			float koef= 0;
+			double koef= 0;
 			//делаю mas[2][0] нулевым. для этого делю mas[2][0] на mas[0][0], получая koef. затем mas[2][j]=mas[0][j]*koef-mas[2][j]. т.о. в mas[2][0] будет 0. 
 			//примечание. если koef 0, решение будет опускаться
 			//счёт 3й строки
